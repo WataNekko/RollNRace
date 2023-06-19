217e442d7eb0b84279721e94dec41b0a689e737c
WataNekko <88185666+WataNekko@users.noreply.github.com>
2023-06-20 04:08:29 +0700

    Add MoveSteps to Player script and keep only coroutines
    It's better for caller to directly call the coroutines instead of call
    the start coroutine wrapper of MoveTo

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,30 +8,51 @@ using UnityEngine.PlayerLoop;
 /// </summary>
 public class Player : MonoBehaviour
 {
+    #region Properties
+    /// <summary>
+    /// The player's current position on the rock path (game board). From 0 to 36, 0 is the starting point before the 1st rock, 36 is the finishing point after the last rock.
+    /// </summary>
+    [field: Header("Properties")]
+    [field: SerializeField]
+    public int CurrentPosition { get; private set; } = 0;
+    /// <summary>
+    /// Whether the player has finished the game
+    /// </summary>
+    public bool IsFinished => CurrentPosition > 35;
+    /// <summary>
+    /// The number of turns the player has played
+    /// </summary>
+    [field: SerializeField]
+    public int TurnCount { get; set; } = 0;
+    /// <summary>
+    /// The number of bonus sectors the player has entered
+    /// </summary>
+    [field: SerializeField]
+    public int BonusSectorCount { get; set; } = 0;
+    /// <summary>
+    /// The number of fail sectors the player has entered
+    /// </summary>
+    [field: SerializeField]
+    public int FailSectorCount { get; set; } = 0;
+    #endregion
+
+
+
+    [Header("Movement")]
     [SerializeField]
     [Tooltip("The maximum height of the hop effect during movement.")]
     private float hopHeight = 0.5f;
 
-    private Coroutine moveCoroutine;
+
+
+    #region Methods
 
     /// <summary>
-    /// Moves the GameObject to the specified position over a given duration, with a little hopping animation.
+    /// A coroutine that moves the GameObject to the specified position over a given duration, with a little hopping animation.
     /// </summary>
     /// <param name="targetPosition">The target position to move towards.</param>
     /// <param name="duration">The duration of the movement in seconds.</param>
-    /// <param name="callback">Optional callback function to invoke when the movement is complete.</param>
-    public void MoveTo(Vector3 targetPosition, float duration = 0.5f, Action callback = null)
-    {
-        if (moveCoroutine != null)
-        {
-            // If a movement coroutine is already running, stop it
-            StopCoroutine(moveCoroutine);
-        }
-
-        moveCoroutine = StartCoroutine(MoveCoroutine(targetPosition, duration, callback));
-    }
-
-    private IEnumerator MoveCoroutine(Vector3 targetPosition, float duration, Action callback)
+    public IEnumerator MoveTo(Vector3 targetPosition, float duration = 0.5f)
     {
         var startPosition = transform.position;
 
@@ -65,11 +86,26 @@ public class Player : MonoBehaviour
         // Ensure the final transform is set accurately
         transform.position = targetPosition;
         transform.rotation = targetRotation;
-
-        // Reset the moveCoroutine reference
-        moveCoroutine = null;
-
-        callback?.Invoke();
     }
+
+    public IEnumerator MoveSteps(int steps)
+    {
+        if (steps < 1)
+        {
+            yield break;
+        }
+
+        var prevPos = CurrentPosition;
+        CurrentPosition = Mathf.Min(CurrentPosition + steps, RockPath.Instance.Length - 1);
+
+        for (int i = prevPos + 1; i <= CurrentPosition; i++)
+        {
+            var pos = RockPath.Instance.GetRock(i).position;
+            pos.y += 0.00225f; // offset to stand on top of the rock
+            yield return MoveTo(pos);
+        }
+    }
+
+    #endregion
 }
 
