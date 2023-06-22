52ae68ed5385844ce67fcb2d167b8f0cdeca1dd0
WataNekko <88185666+WataNekko@users.noreply.github.com>
2023-06-22 07:42:59 +0700

    Decrease dice rolling timeout to 10s

--- Assets/Scripts/Dice.cs
+++ Assets/Scripts/Dice.cs
@@ -54,7 +54,7 @@ public class Dice : Singleton<Dice>
                 ForceMode.Impulse);
 
         // wait for the dice to stop moving
-        var timeout = Time.time + 15f; // 15-second timeout
+        var timeout = Time.time + 10f; // 10-second timeout
         yield return new WaitForSeconds(0.2f);
         yield return new WaitUntil(() => rb.velocity.magnitude == 0f ||
                 Time.time >= timeout);
