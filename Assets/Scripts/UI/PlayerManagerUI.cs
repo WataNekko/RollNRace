11850f56b923c6c54603720c62655adadf84147c
WataNekko <88185666+WataNekko@users.noreply.github.com>
2023-06-22 16:24:55 +0700

    Update adding and removing player that remains selected

--- Assets/Scripts/UI/PlayerManagerUI.cs
+++ Assets/Scripts/UI/PlayerManagerUI.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.UI;
 
 public class PlayerManagerUI : Singleton<PlayerManagerUI>
 {
@@ -23,10 +22,11 @@ public class PlayerManagerUI : Singleton<PlayerManagerUI>
     [SerializeField]
     private PlayerSelector playerSelectorPrefab;
 
-    private void AddPlayerToEditList(Player player)
+    private PlayerSelector AddPlayerToEditList(Player player)
     {
         var p = Instantiate(playerSelectorPrefab, chooserContent);
         p.Init(player);
+        return p;
     }
 
     private void Start()
@@ -52,14 +52,23 @@ public class PlayerManagerUI : Singleton<PlayerManagerUI>
         // minimum 2 players
         if (PlayerManager.Instance.PlayerCount <= 2) return;
 
+        var parent = SelectedPlayer.transform.parent;
+        var selectedIndex = SelectedPlayer.transform.GetSiblingIndex();
+        var nextSelectedIndex = selectedIndex == parent.childCount - 1
+                // is last child
+                ? selectedIndex - 1
+                : selectedIndex + 1;
+        var nextSelected = parent.GetChild(nextSelectedIndex);
+
+
         Destroy(SelectedPlayer.Player.gameObject);
         Destroy(SelectedPlayer.gameObject);
-        SelectedPlayer = null;
+        SelectedPlayer = nextSelected.GetComponent<PlayerSelector>();
     }
 
     public void AddPlayer()
     {
         var newPlayer = PlayerManager.Instance.AddPlayer();
-        AddPlayerToEditList(newPlayer);
+        SelectedPlayer = AddPlayerToEditList(newPlayer);
     }
 }
